5c24ca606e4659faaa3cd8c56dc01f6e31685f27
Alexander Jansson <Alexanderjan94@gmail.com>
2016-09-15 12:59:57 +0200

    asdasd

--- Pages/Script/homeJavaScript.js
+++ Pages/Script/homeJavaScript.js
@@ -1,12 +1,9 @@
 var imgArray=["Bilder/covers.png", "Bilder/kontor-3.png" , "Bilder/kontor-2.png"];
 
-//
-//function displayImage(){
 
-//var num= Math.floor(Math.random()* 3); 
-//document.canvas.src=imgArray[num];
-//}
+function displayImage(){
 
-$(document).ready(function ()
-{
-});
+var num= Math.floor(Math.random()* 3); 
+document.canvas.src=imgArray[num];
+}
+
