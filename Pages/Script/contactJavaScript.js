3dd17d644831dee8d880b80a1d9910df1ec8e2bd
Alexander Jansson <Alexanderjan94@gmail.com>
2016-09-16 02:45:04 +0200

    asdasd

---
+++ Pages/Script/contactJavaScript.js
@@ -0,0 +1,12 @@
+$('#submit').click(function() {
+$.ajax({
+url: 'http://ofcourse.oru.se/~IK2009/contactform/contact_form.php',
+type: 'GET',
+success: function(msg) {
+alert(msg);
+},
+error: function() {
+alert('Nu var det något som gick fel :(');
+}
+});
+});
--- Pages/Script/homeJavaScript.js
+++ Pages/Script/homeJavaScript.js
@@ -1,9 +1,20 @@
-var imgArray=["Bilder/covers.png", "Bilder/kontor-3.png" , "Bilder/kontor-2.png"];
 
 
-function displayImage(){
 
-var num= Math.floor(Math.random()* 3); 
-document.canvas.src=imgArray[num];
-}
+var imgArray = ["Bilder/kontor-1.png", "Bilder/kontor-3.png" , "Bilder/kontor-2.png"];
+       loopImgArray();
+       var interval = setInterval(loopImgArray,15000);
+            
 
+       function loopImgArray(){
+        for(var i = 0; i < imgArray.length;  i++){
+        	(function(i){
+ setTimeout(function(){
+           document.canvas.src=imgArray[i];
+},i*4000);
+   })(i);
+
+        }	
+        	
+        }
+       
