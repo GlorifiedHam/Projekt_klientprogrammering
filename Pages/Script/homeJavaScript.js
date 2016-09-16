9f0a082d52e5445245ba50cdcf5b20f9ff4bfdf7
joachim johnson <joachim@johnsons.nu>
2016-09-16 09:04:18 +0200

    a
    a

--- Pages/Script/homeJavaScript.js
+++ Pages/Script/homeJavaScript.js
@@ -1,20 +1,16 @@
-
-
-
 var imgArray = ["Bilder/kontor-1.png", "Bilder/kontor-3.png" , "Bilder/kontor-2.png"];
        loopImgArray();
        var interval = setInterval(loopImgArray,15000);
-            
+        
+function loopImgArray(){
+    for (var i = 0; i < imgArray.length; i++) {
 
-       function loopImgArray(){
-        for(var i = 0; i < imgArray.length;  i++){
-        	(function(i){
- setTimeout(function(){
-           document.canvas.src=imgArray[i];
-},i*4000);
-   })(i);
-
-        }	
+        	    (function(i){
+                 setTimeout(function(){
+                           document.canvas.src=imgArray[i];
+                },i*4000);
+                   })(i);
+     }	
         	
-        }
+}
        
