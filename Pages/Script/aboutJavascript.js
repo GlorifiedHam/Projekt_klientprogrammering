a2cbb04fc829ff4e49cfe1c3e4fb1a4a9762ac45
Alexander Jansson <Alexanderjan94@gmail.com>
2016-09-16 12:26:52 +0200

    asdasd

--- Pages/Script/aboutJavascript.js
+++ Pages/Script/aboutJavascript.js
@@ -1,16 +1,64 @@
-$.getJSON(
+ $(document).ready(function(){
+
+
+$.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',function (data) {
+if (data.status == 1) { // Lyckades
+// kommer hit.
+
+
+$('#output').append(
+'<br />' + data.result.orebro.name + data.result.orebro.address + data.result.orebro.phone + '<br />'+
+'<br />' + data.result.goteborg.name + data.result.goteborg.address + data.result.goteborg.phone + '<br />'+
+'<br />' + data.result.toronto.name + data.result.toronto.address + data.result.toronto.phone + '<br />'+
+'<br />' + data.result.snoqualmie.name + data.result.snoqualmie.address + data.result.snoqualmie.phone + '<br />'+
+'<br />' + data.result.sheffield.name + data.result.sheffield.address + data.result.sheffield.phone + '<br />'
+
+
+);
+
+}
+else { // Misslyckades
+ Felmeddelande:
+$('#output').text(data.message);
+};
+
+});
+
+
+
+functionMapClick(city){
+
+	$.getJSON(
 'http://ofcourse.oru.se/~IK2009/json/get_office.php',
-{},
+{city},
 function (data) {
 if (data.status == 1) { // Lyckades
 // Resultat:
-for (var i = 0; i < data.result.length; i++) {
-	$('#output').append(
-data.result[i].name + data.result[i].adress + data.result[i].phone + '<br />')
-};
+$('#mapAnswer').text(data.result.city);
 } else { // Misslyckades
 // Felmeddelande:
-$('#output').text(data.message);
+$('#mapAnswer').text(data.message);
 }
 }
 );
+
+
+}
+
+});
+/* var html = '';
+$.each(data, function(entryIndex, entry){
+html+='<div class = "entry">';
+html+='<h3 class = name>' + entry.name + '<h3>';
+html+='<div class = adress>' + entry.adress + '</div>';
+html+='<div class = phone>' + entry.phone + '</div>';
+html += '</div>';
+console.log(data.result[entryIndex].adress) ;
+});
+$('#output').html(html);
+
+
+}
+*/
+
+
