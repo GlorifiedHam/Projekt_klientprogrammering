57d47deaafc82e6b2128675b2aaebdc83a115cb8
Alexander Jansson <Alexanderjan94@gmail.com>
2016-09-16 14:12:37 +0200

    asdasd

--- Pages/Script/aboutJavascript.js
+++ Pages/Script/aboutJavascript.js
@@ -1,6 +1,5 @@
  $(document).ready(function(){
 
-$('.foo').maphilight();
 
 $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',function (data) {
 if (data.status == 1) { // Lyckades
@@ -25,9 +24,7 @@ $('#output').text(data.message);
 
 });
 
-  $(function () {
-        $('.foo').maphilight({ stroke: false, fillColor: '009DDF', fillOpacity: 1 });
-    });
+
 
 });
 
