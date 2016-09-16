2190a206b71ecc3f20432f387f8987c36f5c3ae2
joachim johnson <joachim@johnsons.nu>
2016-09-16 09:13:59 +0200

    a
    a

--- Pages/Script/homeJavaScript.js
+++ Pages/Script/homeJavaScript.js
@@ -1,16 +1,18 @@
 var imgArray = ["Bilder/kontor-1.png", "Bilder/kontor-3.png" , "Bilder/kontor-2.png"];
-       loopImgArray();
-       var interval = setInterval(loopImgArray,15000);
+var interval = setInterval(loopImgArray, 15000);
+
+loopImgArray(); //startar bildspelet på inmatning av javascript
         
-function loopImgArray(){
+function loopImgArray() {
+    //javascripten för bildspelet. vi använde javascript och INTE jqerry
     for (var i = 0; i < imgArray.length; i++) {
 
-        	    (function(i){
-                 setTimeout(function(){
-                           document.canvas.src=imgArray[i];
-                },i*4000);
-                   })(i);
+        (function (i) {
+            setTimeout(function(){
+                 document.canvas.src=imgArray[i];
+            }, i * 4000);
+        })(i); //end of the timeout loop
      }	
-        	
-}
+
+}// slutet på bildspelets function
        
