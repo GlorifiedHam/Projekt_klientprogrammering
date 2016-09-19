fa2512c7ffe28aff96ba1fe43de339fd5026b9ce
A.Jansson <AlexanderJan94@gmail.com>
2016-09-19 12:38:14 +0200

    School fixes

--- Pages/Script/aboutJavascript.js
+++ Pages/Script/aboutJavascript.js
@@ -1,32 +1,76 @@
  $(document).ready(function(){
-
-
+ 
     $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',function (data) {
         if (data.status == 1) { // Lyckades att hämta
-        // kommer hit.
+        // kommer hit.      
+for (var prop in data.result) {
+  if( data.result.hasOwnProperty( prop ) ) {
+    $('#output').append(
+           'Land: ' +data.result[prop].name + 
+          'Address: ' + data.result[prop].address + 
+          'Phone: ' + data.result[prop].phone +'<br />'
 
-            $('#output').append(
-                data.result.orebro.name +' Adress:  '+  data.result.orebro.address +' Phone: '+ data.result.orebro.phone + '<br />'+
-                '<br />' + data.result.goteborg.name + ' Adress: ' + data.result.goteborg.address +'  Phone: '+ data.result.goteborg.phone + '<br />'+
-                '<br />' + data.result.toronto.name + ' Adress: ' +data.result.toronto.address + ' Phone: ' +data.result.toronto.phone + '<br />'+
-                '<br />' + data.result.snoqualmie.name +'  Adress: '+ data.result.snoqualmie.address +'  Phone: '+ data.result.snoqualmie.phone + '<br />'+
-                '<br />' + data.result.sheffield.name +'  Adress: '+ data.result.sheffield.address +' Phone: '+ data.result.sheffield.phone + '<br />'
-            );
+        )
+ 
+  
+  } 
+
+}
         }
         else { // Misslyckades att hämta full listan
-             Felmeddelande:
+             //felmeddelande
             $('#output').text(data.message);
         };
 
+});
+
+});
+
+ $(document).ready(function(){
+
+
         $(".Svergie").on("click", function (e) {
             e.preventDefault();
-            alert("Svergie");
+            
+     $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',{city:'orebro'},function (data) {
+
             if (data.status == 1) { // Lyckades att hämta enskilt kontor
-                $('#mapAnswer').append(
-                    data.result.orebro.name + ' Adress:  ' + data.result.orebro.address + ' Phone: ' + data.result.orebro.phone + '<br />' +
-                    '<br />' + data.result.goteborg.name + ' Adress: ' + data.result.goteborg.address + '  Phone: ' + data.result.goteborg.phone + '<br />'
-                );
+            
+
+                
+                    $('#mapAnswer').append(
+'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
+                        )
+
+
+
             }
+
+
+            else { // Misslyckades att hämta enskilt kontor
+                alert("misslykades att hämta kontor");
+            };
+        });
+    });
+
+        $(".Svergie").on("click", function (e) {
+            e.preventDefault();
+            
+     $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',{city:'goteborg'},function (data) {
+
+            if (data.status == 1) { // Lyckades att hämta enskilt kontor
+            
+
+                
+                    $('#mapAnswer').append(
+'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
+                        )
+
+
+
+            }
+
+            
             else { // Misslyckades att hämta enskilt kontor
                 alert("misslykades att hämta kontor");
             };
@@ -35,60 +79,78 @@
 
 
 
+
+
+
     $(".Toronto").on("click", function (e) {
         e.preventDefault();
-        //alert("Toronto");
-        skrivutstad();
+      
+       
+        $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',{city:'toronto'},function (data) {
+
+            if (data.status == 1) { // Lyckades att hämta enskilt kontor
+            
+
+                
+                    $('#mapAnswer').append(
+'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
+                        )
+
+
+
+            }
+            else { // Misslyckades att hämta enskilt kontor
+                alert("misslykades att hämta kontor");
+            };
+        });
     });
 
     $(".Snoqualmie").on("click", function (e) {
         e.preventDefault();
-        alert("Snoqualmie");
+       
+        $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',{city:'snoqualmie'},function (data) {
+
+            if (data.status == 1) { // Lyckades att hämta enskilt kontor
+            
+
+                
+                    $('#mapAnswer').append(
+'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
+                        )
+
+
+
+            }
+            else { // Misslyckades att hämta enskilt kontor
+                alert("misslykades att hämta kontor");
+            };
+        });
     });
 
     $(".Sheffield").on("click", function (e) {
         e.preventDefault();
-        alert("Sheffield");
+        
+        $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',{city:'sheffield'},function (data) {
+
+            if (data.status == 1) { // Lyckades att hämta enskilt kontor
+            
+
+                
+                    $('#mapAnswer').append(
+'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
+                        )
+
+
+
+            }
+            else { // Misslyckades att hämta enskilt kontor
+                alert("misslykades att hämta kontor");
+            };
+        });
     });
-
 });
 
 
 
-/* functionMapClick(city){
-
-	$.getJSON(
-'http://ofcourse.oru.se/~IK2009/json/get_office.php',
-{city},
-function (data) {
-if (data.status == 1) { // Lyckades
-// Resultat:
-$('#mapAnswer').text(data.result.city);
-} else { // Misslyckades
-// Felmeddelande:
-$('#mapAnswer').text(data.message);
-}
-}
-);
-
-
-}
-
-});
- */
-/* var html = '';
-$.each(data, function(entryIndex, entry){
-html+='<div class = "entry">';
-html+='<h3 class = name>' + entry.name + '<h3>';
-html+='<div class = adress>' + entry.adress + '</div>';
-html+='<div class = phone>' + entry.phone + '</div>';
-html += '</div>';
-console.log(data.result[entryIndex].adress) ;
-});
-$('#output').html(html);
-
-
-}
-*/
 
 
