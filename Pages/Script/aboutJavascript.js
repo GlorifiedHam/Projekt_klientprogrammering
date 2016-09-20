b6878c4b655f8e202a2816d79f230113ae08ae43
joachim johnson <joachim@johnsons.nu>
2016-09-20 11:49:06 +0200

    a
    a

--- Pages/Script/aboutJavascript.js
+++ Pages/Script/aboutJavascript.js
@@ -4,24 +4,22 @@
         if (data.status == 1) { // Lyckades att hämta
         // kommer hit.      
             for (var prop in data.result) {
-              if( data.result.hasOwnProperty( prop ) ) {
-                $('#output').append(
-                       'Land: ' +data.result[prop].name + 
-                      'Address: ' + data.result[prop].address + 
-                      'Phone: ' + data.result[prop].phone +'<br />'
-
-                    )
-              } 
+               if( data.result.hasOwnProperty( prop ) ) {
+                  $('#output').append(
+                         'Land: ' + data.result[prop].name + 
+                         'Address: ' + data.result[prop].address + 
+                         'Phone: ' + data.result[prop].phone +'<br />'
+                  )
+               }
             }
         }
+
         else { // Misslyckades att hämta full listan
              //felmeddelande
             $('#output').text(data.message);
         };
     });
-});
 
- $(document).ready(function(){
 
    $(".Svergie").on("click", function (e) {
             e.preventDefault();
@@ -57,28 +55,17 @@
         });
     });
 
-
-
-
-
-
     $(".Toronto").on("click", function (e) {
         e.preventDefault();
-      
-       
         $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',{city:'toronto'},function (data) {
 
             if (data.status == 1) { // Lyckades att hämta enskilt kontor
-            
 
-                
                     $('#mapAnswer').append(
-'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
-                        )
-
-
-
+                        'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
+                    )
             }
+
             else { // Misslyckades att hämta enskilt kontor
                 alert("misslykades att hämta kontor");
             };
@@ -91,16 +78,12 @@
         $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',{city:'snoqualmie'},function (data) {
 
             if (data.status == 1) { // Lyckades att hämta enskilt kontor
-            
 
-                
                     $('#mapAnswer').append(
-'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
-                        )
-
-
-
+                        'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
+                    )
             }
+
             else { // Misslyckades att hämta enskilt kontor
                 alert("misslykades att hämta kontor");
             };
@@ -113,15 +96,9 @@
         $.getJSON('http://ofcourse.oru.se/~IK2009/json/get_office.php',{city:'sheffield'},function (data) {
 
             if (data.status == 1) { // Lyckades att hämta enskilt kontor
-            
-
-                
                     $('#mapAnswer').append(
-'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
-                        )
-
-
-
+                        'Stad: ' + data.result.name + ' Address: ' +data.result.address +' Phone: ' +data.result.phone +'</br>' 
+                    )
             }
             else { // Misslyckades att hämta enskilt kontor
                 alert("misslykades att hämta kontor");
