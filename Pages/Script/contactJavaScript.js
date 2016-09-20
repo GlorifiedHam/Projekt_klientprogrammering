1a036f936cb6f03e5c80f013e8079903b3dade13
Alexander Jansson <Alexanderjan94@gmail.com>
2016-09-20 12:49:12 +0200

    asdas

--- Pages/Script/contactJavaScript.js
+++ Pages/Script/contactJavaScript.js
@@ -1,19 +1,54 @@
 $(document).ready(function() { 
+	function checkIfEmpty(data){
+return /^\s*$/.test(data);
+		
+}
+
+function validateEmail(data){
+
+var reg =/^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z0-9_\-\.]){1,}\.([A-Za-z]{2,4})$/;
+return reg.test(data);
+//^= start with
+//$= end with
+
+}
 
 $('#submit').click(function () {
 
+if(checkIfEmpty(document.forms["form1"]["Epost"].value)){
+alert("Email kan inte vara tomt" /*lägg till autofocus*/)
+
+}
+
+else if(checkIfEmpty(document.forms["form1"]["amne"].value)){
+alert("Ämne kan inte vara tomt" /*lägg till autofocus*/ )
+}
+
+else if(checkIfEmpty(document.forms["form1"]["fraga"].value)){
+	alert("Du måste ange en fråga ")
+}
+
+else if(!validateEmail(document.forms["form1"]["Epost"].value)){
+alert("Fel format på emailen ")
+
+}
+ else
     $.ajax({
         url: 'http://ofcourse.oru.se/~IK2009/contactform/contact_form.php',
         type: 'GET',
 
         success: function(msg) {
-        alert("msg");
+        alert(msg);
         },
 
         error: function() {
             alert('Nu var det något som gick fel :(');
         }
     });
-});
 
 });
+});
+
+
+
+
