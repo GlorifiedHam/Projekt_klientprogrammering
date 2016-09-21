a02b9080f14bacca5e60f944069739ad17214392
joachim johnson <joachim@johnsons.nu>
2016-09-21 14:47:41 +0200

    a
    a

--- Pages/Script/newsJavaScript.js
+++ Pages/Script/newsJavaScript.js
@@ -1,27 +1,21 @@
-$(document).ready(function()
+
+//document.ready function som får jqerry att vänta tills domen är färdig
+$(document).ready(function ()
 {
+    //.get function som hämtar ut nyheterna från en länk på nätet i en php fil.
+    $.get('http://ofcourse.oru.se/~IK2009/xml/get_news.php', function (data) {//här så tar vi och laddar in allt i data
+        $(data).find('article').each(function(){ //här så tar vi och letar efter classen artikel och för vare gång den hittar det loppar den igenom functionen
+            var $article = $(this); //laddar in det man hittar i article
 
-$.get('http://ofcourse.oru.se/~IK2009/xml/get_news.php',function(data){
-$(data).find('article').each(function(){
-var $article= $(this);
+            var html ='<h2 class="newsTitle">'; 
+            html += $article.find('title').text(); //lägger in rubriken från artikeln i en h2 tag
+            html += '</h2><p class="newsBody">';
+            html += $article.find('bodytext').text(); //lägger in strötexten från artikeln i en p tag
+            html += '</p><p class="newsDate">';
+            html += $article.find('date').text(); //lägger in datumet från artikeln i en p tag
+            html += '</p></div>';
 
- 
-var html ='<h2 class="newsTitle">';
-html += $article.find('title').text(); 
-html += '</h2><p class="newsBody">';
-html += $article.find('bodytext').text();
-html += '</p><p class="newsDate">';
-html += $article.find('date').text();
-html += '</p></div>';
- 
-
-
- $('#news').append($(html));
-
+            $('#news').append($(html)); //här man säter in det i diven news
+        });
+    });
 });
-
-
-
-});
-
-	});
